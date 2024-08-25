16a07e4fb72270e26a9df594edc8899413ec0d73
radman1007 <radman.cod110@gmail.com>
2024-08-25 17:24:18 +0330

    debug sum_of_income

--- querashop_queries/store/queries.py
+++ querashop_queries/store/queries.py
@@ -1,4 +1,9 @@
 from . import models
+from django.utils import timezone
+from datetime import timedelta
+from django.db.models import Count
+from django.db.models import Sum
+
 
 def young_employees(job: str):
     query = models.Employee.objects.filter(age__lt=30).filter(job=job)
@@ -14,12 +19,21 @@ def products_sold_by_companies():
 
 
 def sum_of_income(start_date: str, end_date: str):
-    query = models.Order.objects.filter(time__range=[start_date, end_date])
+    query = models.Order.objects.filter(time__range=[start_date, end_date]).aggregate(total=Sum('price'))
+    print(query)
+    return query['total'] or 0
 
 
 def good_customers():
-    pass
-
+    one_month_ago = timezone.now() - timedelta(days=30)
+    query = models.Customer.objects.filter(level="G"
+                                ).filter(order__time__gte=one_month_ago
+                            ).annotate(order_count=Count('orders')
+                        ).filter(order_count__gt=10
+                    ).values('name', 'phone')
+                        
+    for customer in query:
+        print((customer['name'], customer['phone']))
 
 def nonprofitable_companies():
     pass
