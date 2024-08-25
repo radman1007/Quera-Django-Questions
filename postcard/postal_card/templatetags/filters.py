c43a2817674cbc2f75a8d52e709cd56dd11cc7db
radman1007 <radman.cod110@gmail.com>
2024-08-25 17:36:18 +0330

    postcard question finished

--- postcard/postal_card/templatetags/filters.py
+++ postcard/postal_card/templatetags/filters.py
@@ -1,1 +1,9 @@
-# Put you code here
+from django import template
+
+register = template.Library()
+
+@register.filter
+def translate_number(value):
+    value = str(value)
+    english_to_persian_table = value.maketrans('0123456789', '۰۱۲۳۴۵۶۷۸۹')
+    return value.translate(english_to_persian_table)
