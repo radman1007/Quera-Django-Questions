70c7f5384d3807e812d2c9178e20aa570c5a7d63
radman1007 <radman.cod110@gmail.com>
2024-08-26 01:54:31 +0330

    finished forms.py of jalali question

--- jalali calender/Users/forms.py
+++ jalali calender/Users/forms.py
@@ -1,5 +1,26 @@
 from django import forms
+from .models import CustomUser
+from django.core.exceptions import ValidationError
 
 
 class CustomUserForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+    
+    def clean_national_code(self):
+        national_code = self.changed_data.get('national_code')
+        if len(national_code) == 10:
+            return national_code
+        else:
+            raise ValidationError('national code must be 10 character')
+        
+    def clean_fullname(self):
+        fullname = self.cleaned_data.get('fullname')
+        parts = fullname.split(' ')
+        if len(parts) != 2:
+            raise ValidationError('you must use space in fullname')
+        first_name, last_name = parts
+        if not (first_name.istitle() and last_name.istitle()):
+            raise ValidationError("you must use big letter for first character")
+        return fullname
--- jalali calender/Users/models.py
+++ jalali calender/Users/models.py
@@ -1,5 +1,33 @@
 from django.db import models
+import jdatetime
 
 
 class CustomUser(models.Model):
-    pass
+    GENDER_BOX = {
+        'M' : 'Male',
+        'F' : 'Female',
+    }
+    username = models.CharField(max_length=256)
+    fullname = models.CharField(max_length=256)
+    gender = models.CharField(max_length=1, choices=GENDER_BOX)
+    national_code = models.CharField(max_length=10)
+    # birthday_date = 
+    # ceremony_datetime = 
+    country = models.CharField(max_length=256 ,default="Iran")
+
+    def __str__(self):
+        return self.username
+
+    def get_first_and_last_name(self):
+        fullname = self.fullname.split(' ')
+        if len(fullname) == 2:
+            first_name, last_name = fullname
+            return {'first_name' : first_name, 'last_name':last_name}
+        else:
+            return False
+
+    def is_birthday(self):
+        if self.birthday_date == jdatetime.date.today():
+            return True
+        else:
+            return False
