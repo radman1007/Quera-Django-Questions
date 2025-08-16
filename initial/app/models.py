edc59b380a21059ff174f1684771474c8f18c015
radman1007 <radman.cod110@gmail.com>
2025-08-17 00:00:08 +0330

    answer the question

--- initial/app/models.py
+++ initial/app/models.py
@@ -1,17 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class RoleType(models.IntegerChoices):
-    pass
+    MEMBER = 0, "member"
+    OWNER = 1, "owner"
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="children", null=True, blank=True)
 
 
 class Project(models.Model):
-    pass
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="projects", null=True)
+    members = models.ManyToManyField(User, through="Membership")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        ordering = ("updated_at",)
+    
 
 
 class Membership(models.Model):
-    pass
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, models.CASCADE)
+    role = models.IntegerField(choices=RoleType.choices, default=RoleType.MEMBER)
+    
+    
+    class Meta:
+        unique_together = ("person", "project")
--- initial/app/views.py
+++ initial/app/views.py
@@ -1,5 +1,10 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ProjectSerializer
+from .models import Project
 
 
 class ProjectListView(ListAPIView):
-    pass
+    permission_classes = (IsAuthenticated, )
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
